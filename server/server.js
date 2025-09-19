fd44a916eb588a2a3b25660a6250aa59b2f9e2f0
PD <ppatel3735@gmail.com>
2025-09-19 18:05:04 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -23,10 +23,25 @@ const PORT = process.env.PORT || 5000;
 
 // --- Database Connection ---
 connectDB();
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000", "https://cafechain-livid.vercel.app"];
 
 // --- Core Middleware ---
+// app.use(cors({
+//     origin: ["http://localhost:5173", "http://localhost:3000"],
+//     credentials: true,
+// }));
+
+
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: (origin, callback) => {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true,
 }));
 
